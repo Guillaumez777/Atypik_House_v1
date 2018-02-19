d22e398c248d995e7358c877b1f2ab17b6811d10
Trinidad Gilbert <gilbert.trinidad1@gmail.com>
2018-02-19 20:52:16 +0100

    remove root password and change ergonomy route file

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,17 @@ Route::get('/', function () {
     return view('subscribe');
 });*/
 
-
-Route::get('/houses/index', 'HousesController@index');
+//Gestion du CRUD hebergements
 Route::resource('houses', 'HousesController', ['only' => ['index','show', 'create', 'store', 'update', 'destroy']]);
+
+//Gestion de l'authentification de l'utilisateur
 Route::get('/register', 'RegistersController@create');
 Route::post('/register', 'RegistersController@store');
 Route::post('/login', 'SessionsController@login');
 Route::get('/verifyemail/{token}', 'Auth\RegisterController@verify');
 Auth::routes();
 
+//Page d'accueil
 Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
