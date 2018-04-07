180964765765c0d16273d49a292923021fb5196b
Guillaume <montagne.guillaume@yahoo.fr>
2018-04-07 12:13:01 +0200

    Modif champ recherche

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="title title-intro">Trouvez les meilleurs locations insolites en Europe !</h1>
-                    <div class="col-lg-6 col-lg-offset-3">
+                    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-9 col-sm-offset-1">
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Saisir une ville">
                             <span class="input-group-btn">
