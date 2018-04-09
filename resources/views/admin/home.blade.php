2a2bfc789279ef787dcc8a465854f7b30a1d3b25
Guillaume <montagne.guillaume@yahoo.fr>
2018-04-09 13:08:24 +0200

    ajout des hébergements

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,7 +2,33 @@
 @section('content')
 <div id="hebergement">
     <h2>Hebergement</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus vitae nulla nemo, ipsa, officiis recusandae praesentium nihil, minus cumque nostrum enim aliquam nam libero earum mollitia eaque. Voluptas, cum esse!</p>
+    <div class="row">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Titre : </th>
+                    <th>Prix : </th>
+                    <th>Actions : </th>
+                </tr>
+            </thead>
+        </table>
+    @foreach($houses as $house)
+    <div class="table-responsive">
+        <table class="table">
+            <tbody>
+                <tr>
+                    <td><a href="{{action('HousesController@show', $house['id'])}}">{{$house->title}}</a></td>
+                    <td>{{$house->price}}€</td>
+                    <td>
+                        <a href="#" class="btn btn-warning">Modifier</a>
+                        <a href="#" class="btn btn-danger">Supprimer</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    @endforeach
+    </div>
 </div>
 <div id="reservation">
     <h2>Reservation</h2>
