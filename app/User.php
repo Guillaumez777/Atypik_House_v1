21240c8d2d1118f881eade5d8371b7ecdc2f4467
gigi603 <gilbert.trinidad1@gmail.com>
2018-05-14 12:03:06 +0200

    problem

--- app/User.php
+++ app/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
     }
     public function comments() {
         return $this->hasMany('App\Comment');
-
+    }
     public function isAdmin()
     {
         return $this->admin; // this looks for an admin column in your users table
