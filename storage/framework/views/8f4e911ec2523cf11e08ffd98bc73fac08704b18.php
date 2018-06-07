adc30e96ab8d2fe579dd5063342be272edda1dc8
Guillaume <g.montagne777@gmail.com>
2018-06-07 19:41:12 +0200

    Recup master Guillaumee

--- storage/framework/views/8f4e911ec2523cf11e08ffd98bc73fac08704b18.php
+++ storage/framework/views/8f4e911ec2523cf11e08ffd98bc73fac08704b18.php
@@ -4,33 +4,57 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="title title-intro">Trouvez les meilleurs locations insolites en Europe !</h1>
-                    <div class="col-lg-6 col-lg-offset-3">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Saisir une ville">
-                            <span class="input-group-btn">
-                                <button class="btn btn-searchbar" type="button">Rechercher</button>
-                            </span>
-                        </div>
+                    <div class="input-group">
+                        <span class="input-group-btn">
+                            <form class="form-horizontal" method="get" action="<?php echo e(url('search')); ?>" enctype="multipart/form-data">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <h1 class="title title-intro">Trouvez les meilleurs locations insolites en Europe !</h1>
+                                        <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-9 col-sm-offset-1">
+                                            <div class="form-group button2">
+                                                <?php echo $__env->make('search',['url'=>'search','link'=>'search'], array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </span>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
+                            <!--
+                            <?php echo Form::open(['method'=>'GET','url'=>'QueryController@search','class'=>'form','role'=>'search']); ?>
+
+                            <?php echo Form::text('search', null,
+                                                array('required',
+                                                        'class'=>'form-control ',
+                                                        'placeholder'=>'Saisir une ville ...')); ?>
+
+                            <?php echo Form::submit('Rechercher',
+                                                        array('class'=>'btn btn-searchbar')); ?>
+
+                            <?php echo Form::close(); ?>-->
 <div class="container list-category">
     <h2>Nos hébergements</h2>
     <div class="row">
       <?php $__currentLoopData = $houses; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $house): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <div class="col-lg-4 col-md-6 mb-4">
+        <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
             <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="<?php echo e(asset('img/houses/'.$house->photo)); ?>"></a>
+                <a href="<?php echo e(action('HousesController@show', $house['id'])); ?>"><img class="img-responsive" src="<?php echo e(asset('img/houses/'.$house->photo)); ?>"></a>
                 <div class="card-body">
                     <h3 class="title card-title">
-                        <a href="<?php echo e(action('HousesController@show', $house['id'])); ?>"><?php echo e($house->title); ?></a>
+                        <a href="<?php echo e(action('HousesController@show', $house['id'])); ?>"><?php echo e($house->title); ?></a>   
                     </h3>
+                    <p>Type de bien : Logement</p>
+                    <p><i class="fas fa-bed"></i> : 2 lits - <i class="fas fa-users"></i> : pour 2 Personnes</p>
                     <h3 class="price"><?php echo e($house->price); ?>€</h3>
-                    <p class="card-text"><?php echo e($house->description); ?></p>
+                    <p class="card-text"><?php echo(substr($house->description, 0, 50));?></p>
+                    <p> <?php echo e($house->ville->ville_nom); ?></p>
                 </div>
                 <div class="note card-footer">
                     <medium class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</medium>
@@ -39,21 +63,6 @@
             </div>
         </div>   
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-    </div>
-</div>
-<div class="newsletter-block">
-    <div class="row">
-        <div class="col-lg-12">
-            <h3 class="title">Abonnez-vous à notre newsletter afin d'être au courant des dernières nouveautés</h3>
-            <div class="col-lg-6 col-lg-offset-3">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Saisir une ville">
-                    <span class="input-group-btn">
-                        <button class="btn btn-default btn-searchbar" type="button">Rechercher</button>
-                    </span>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 <?php $__env->stopSection(); ?>
