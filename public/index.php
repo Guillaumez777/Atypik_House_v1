424a73be5de0df75e907971c1ba0e24b3964c52e
gigi603 <gilbert.trinidad1@gmail.com>
2018-09-19 10:20:15 +0200

    connect api to ionic

--- public/index.php
+++ public/index.php
@@ -21,6 +21,7 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
 require __DIR__.'/../bootstrap/autoload.php';
 
 /*
