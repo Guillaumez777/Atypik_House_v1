6ac32a2dc45d67e67e9cf37ede170d83460bbf55
gigi603 <gilbert.trinidad1@gmail.com>
2018-10-08 01:35:20 +0200

    Removed all files that are in the .gitignore

--- storage/framework/views/65b2675b5bfd37e5bbfc9f284c931b310678a579.php
+++
@@ -1,4 +0,0 @@
-<h1>Bienvenue <?php echo e($prenom); ?></h1>
-<p>Cliquez sur le lien afin de valider <a href="<?php echo e(url('/users/confirmation'.$email_token)); ?>">votre compte</a></p>
-<p>Une fois votre compte validée, connectez-vous sur <a href="<?php echo e(url('/login')); ?>">Atypikhouse</a></p>
-<p>Notre équipe vous remercie.</p>
--- storage/framework/views/75aecfca9b35a8eb9d17b82d7130ceaba4a6d22f.php
+++
@@ -1,5 +0,0 @@
-<?php $__currentLoopData = $user; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $profile): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <p>Nom <?php echo e($profile->nom); ?></p>
-    <p>Prenom <?php echo e($profile->prenom); ?></p>
-    <p>Email <?php echo e($profile->email); ?></p>
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
