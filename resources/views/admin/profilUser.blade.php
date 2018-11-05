68dfa9afb8c34aee253e2a3ea53acf4a2626b4b9
gigi603 <gilbert.trinidad1@gmail.com>
2018-11-05 17:07:59 +0100

    margin-top for bloc user in admin dashboard

--- resources/views/admin/profilUser.blade.php
+++ resources/views/admin/profilUser.blade.php
@@ -1,36 +1,42 @@
 @extends('layouts.admin')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Profil de </div>
-                <div class="panel-body">                                       
-                    <div class="form-group">
-                        <label for="name" class="col-md-4 control-label">Nom:</label>
-                        <div class="col-md-6">
-                            Trinidad
+<div class="admin-user-profil">
+    <div class="container">
+        
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Profil de </div>
+                    <div class="panel-body">
+                        <div class="row">                                      
+                            <div class="form-group">
+                                <label for="name" class="col-md-6 control-label">Nom:</label>
+                                <div class="col-md-6">
+                                    Trinidad
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="name" class="col-md-4 control-label">Prénom</label>
-                        <div class="col-md-6">
-                            Gilbert
+                        <div class="row">
+                            <div class="form-group">
+                                <label for="name" class="col-md-6 control-label">Prénom</label>
+                                <div class="col-md-6">
+                                    Gilbert
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="name" class="col-md-4 control-label">Email</label>
-                        <div class="col-md-6">
-                            gilbert.trinidad1@gmail.com
+                        <div class="form-group">
+                            <label for="name" class="col-md-6 control-label">Email</label>
+                            <div class="col-md-6">
+                                gilbert.trinidad1@gmail.com
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <a href="{{ url()->previous() }}" class="btn btn-primary">Retour</a>
-                        </div>                      
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <a href="{{ url()->previous() }}" class="btn btn-primary">Retour</a>
+                            </div>                      
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        
     </div>
 </div>
