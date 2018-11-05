b35519fd354f717178c01965d4f19037dd02b38d
gigi603 <gilbert.trinidad1@gmail.com>
2018-11-05 15:04:24 +0100

    houses migrations

---
+++ resources/views/user/houses.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('link')
+<link href="{{ asset('css/jquery-ui.min.css') }}" rel="stylesheet">
+@section('content')
+<div class="container list-category">
+    <h2>Nos hébergements</h2>
+    @foreach ($houses as $house)
+    <div class="row">
+        <div class="col-lg-6 col-md-6 mb-4">
+            <div class="card h-100">
+                <a href="{{action('HousesController@show', $house['id'])}}"><img class="img-responsive img_house" src="{{ asset('img/houses/'.$house->photo) }}"></a>
+                <div class="card-body">
+                    <h4 class="title card-title">
+                        <a href="#">{{$house->title}}</a>
+                    </h4>
+                    <p>Type de bien : Logement</p>
+                    <p><i class="fas fa-bed"></i> : 2 lits - <i class="fas fa-users"></i> : pour 2 Personnes</p>
+                    <h3 class="price">{{$house->price}}€</h3>
+                    <p class="card-text"><?php echo(substr($house->description, 0, 40));?></p>
+                    @if($house->statut == "En attente de validation")
+                        <p>Statut: <span style="color:red;"><?php echo($house->statut);?></span></p>
+                    @else
+                        <p>Statut: <span style="color:green;"><?php echo($house->statut);?></span></p>
+                    @endif
+                </div>
+            </div> 
+        </div>
+    </div>
+    @endforeach   
+</div>
+@section('script')
+<script src="{{ asset('js/jquery.js') }}"></script>
+<script src="{{ asset('js/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('js/calendar.js') }}"></script>
+@endsection
