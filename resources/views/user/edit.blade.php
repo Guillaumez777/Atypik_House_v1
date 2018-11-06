93b9d7bcaf5792bc407dee9e8293f0697afd5b81
gigi603 <gilbert.trinidad1@gmail.com>
2018-11-07 00:21:22 +0100

    edit house user in user profile

---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Modifier</div>
+                @foreach($houses as $house)
+                <div class="panel-body">
+                    @if ($success = Session::get('success'))
+                        <div class="alert alert-info">
+                            {{ $success }}
+                        </div>
+                    @endif
+                    <form class="form-horizontal" method="POST" action="{{ route('user.updateHouse', $house->id) }}" enctype="multipart/form-data">                      
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="name" class="col-md-4 control-label">Titre</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="title" required autofocus value="{{$house->title}}">
+                            </div>
+                        </div>
+                        
+
+                        <div class="form-group">
+                            <label for="name" class="col-md-4 control-label">Prix</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="price" autofocus value="{{$house->price}}">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="name" class="col-md-4 control-label">Photo</label>
+                            
+                            <div class="col-md-6">
+                                <img class="img-responsive img_house" src="{{ asset('img/houses/'.$house->photo) }}">
+                            </div>
+                        </div>
+                        
+                        <div class="form-group">
+                            <label class="col-md-4"></label>
+                            <div class="col-md-6">
+                            <input id="name" type="file" class="form-control" name="photo" autofocus value="{{$house->photo}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="description" class="col-md-4 control-label">Description</label>
+
+                            <div class="col-md-6">
+                                <textarea class="form-control" name="description" rows="5">{{$house->description}}</textarea>
+                            </div>
+                        </div>
+                        <!--<div class="col-md-6">
+                            <input id="name" type="hidden" class="form-control" name="idUser" autofocus value="{{ Auth::user()->id }}">
+                        </div>-->
+
+@endforeach
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Enregistrer
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
