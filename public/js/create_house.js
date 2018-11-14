e0d7a6ab5b432b3e5e56ac78a3e66adbf7ea3370
gigi603 <gilbert.trinidad1@gmail.com>
2018-11-14 14:33:57 +0100

    display proprietes in create_step2_house dynamically

--- public/js/create_house.js
+++ public/js/create_house.js
@@ -9,16 +9,12 @@ $(document).ready(function(){
                 dataType: "json",
                 data: "",
                 success: function(data) {
-                    
+                    $('.proprietes').empty();
                     console.log(data);
                     for (i in data.proprietes) {
-                        //console.log(data.proprietes[0].propriete);
-                        $('.proprietes').empty();
                         data.proprietes[i].propriete;
-                        $( ".proprietes" ).after("<p>"+data.proprietes[i].propriete+"</p>");
-                    }
-                    
-                    //$( "#select_category" ).after( "<div class='form-group'><label for='name' class='col-md-4 control-label'>{{$propriete->propriete}}</label><div class='col-md-6'><input type='text' class='form-control proprietes' name='price' autofocus value=''></div></div>");
+                        $( ".proprietes" ).append("<div class='form-group'><label for='name' class='col-md-4 control-label'>"+data.proprietes[i].propriete+"</label><div class='col-md-6'><input type='text' class='form-control proprietes' name='value' autofocus value=''></div></div>");
+                    }                    
                 },error: function (data){
                     $('.proprietes').empty();
                 }
