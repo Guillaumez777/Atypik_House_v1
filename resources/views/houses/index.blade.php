3669272e3d9a8ce13449e114e43b60027ec7fd1d
gigi603 <gilbert.trinidad1@gmail.com>
2018-12-27 18:25:12 +0100

    affichage des propriétés et de leur valeur de la house dans la page houses

--- resources/views/houses/index.blade.php
+++ resources/views/houses/index.blade.php
@@ -31,12 +31,8 @@
                             <a href="{{action('UsersController@showHouse', $house['id'])}}">{{$house->title}}</a>
                         </h3>
                         <p>Type de bien : {{$house->category->category}}</p>
-                        @foreach($house->proprietes as $proprietes)
-                            <p>{{$proprietes->propriete}}: 
-                                @foreach($proprietes->valuecatproprietes as $valuepropriete) 
-                                    {{$valuepropriete->value}}
-                                @endforeach
-                            </p>     
+                        @foreach($house->valuecatproprietes as $valuecatpropriete)
+                            <p>{{$valuecatpropriete->propriete->propriete}}: {{$valuecatpropriete->value}}</p>        
                         @endforeach
                         <p class="price">{{$house->price}}€ par nuit</p>
                         <p class="card-text"><?php echo(substr($house->description, 0, 40));?></p>
