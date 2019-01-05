580cb975c53bb62ed756b32530d3d14653f1145f
gigi603 <gilbert.trinidad1@gmail.com>
2019-01-05 14:58:56 +0100

    stripe

--- resources/views/paywithstripe.blade.php
+++ resources/views/paywithstripe.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.app')
 @section('link')
+<script src="https://js.stripe.com/v3/"></script>
 
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet">
+  <link href="{{ asset('css/base_stripe.css') }}" rel="stylesheet">
+<link href="{{ asset('css/stripe.css') }}" rel="stylesheet">
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -84,5 +90,34 @@
             </div>
         </div>
     </div>
+</div> -->
+<div class="container list-category">
+    <div class="panel panel-default">
+        <div class="panel-heading">Récapitulatif de votre réservation</div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card h-100 text-center">
+                        <form action="/charge" method="post" id="payment-form">
+                            <div class="form-row" style="padding-bottom:30px;">
+                                <label for="card-element">
+                                Credit or debit card
+                                </label>
+                                <div id="card-element">
+                                <!-- A Stripe Element will be inserted here. -->
+                                </div>
+
+                                <!-- Used to display form errors. -->
+                                <div id="card-errors" role="alert"></div>
+                            </div>
+                                <button class="btn btn-success btn_reserve">Payer</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<script src="https://js.stripe.com/v3/"></script>
+<script src="{{ asset('js/stripe.js') }}"></script>        
 @endsection
--- storage/framework/views/3465b562d28e618e6cf6a729fb668ec84b448c57.php
+++ storage/framework/views/3465b562d28e618e6cf6a729fb668ec84b448c57.php
@@ -1,7 +1,13 @@
 <?php $__env->startSection('link'); ?>
+<script src="https://js.stripe.com/v3/"></script>
 
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet">
+  <link href="<?php echo e(asset('css/base_stripe.css')); ?>" rel="stylesheet">
+<link href="<?php echo e(asset('css/stripe.css')); ?>" rel="stylesheet">
 <?php $__env->startSection('content'); ?>
-<div class="container">
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -86,7 +92,36 @@
             </div>
         </div>
     </div>
+</div> -->
+<div class="container list-category">
+    <div class="panel panel-default">
+        <div class="panel-heading">Récapitulatif de votre réservation</div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card h-100 text-center">
+                        <form action="/charge" method="post" id="payment-form">
+                            <div class="form-row" style="padding-bottom:30px;">
+                                <label for="card-element">
+                                Credit or debit card
+                                </label>
+                                <div id="card-element">
+                                <!-- A Stripe Element will be inserted here. -->
+                                </div>
+
+                                <!-- Used to display form errors. -->
+                                <div id="card-errors" role="alert"></div>
+                            </div>
+                                <button class="btn btn-success btn_reserve">Payer</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<script src="https://js.stripe.com/v3/"></script>
+<script src="<?php echo e(asset('js/stripe.js')); ?>"></script>        
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
