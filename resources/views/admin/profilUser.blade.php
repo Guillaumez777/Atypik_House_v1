3d4d11f4fd9393a5b81e19f8aa26bd44af3a7594
Guillaume Montagné <g.montagne777@gmail.com>
2019-01-11 19:59:44 +0100

    modif css admin

--- resources/views/admin/profilUser.blade.php
+++ resources/views/admin/profilUser.blade.php
@@ -4,14 +4,14 @@
     <div class="container-fluid">
         <div class="col-md-12">
             <div class="col-md-2"></div>
-            <div class="col-md-3">
+            <div class="col-offset-md-2 col-md-10">
                 <div class="panel panel-default">
                 @foreach ($users as $user)
                     <div class="panel-heading">Profil de {{$user->nom}} {{$user->prenom}}</div>
                     <div class="panel-body">       
                         <div class="row">                                      
                             <div class="form-group">
-                                <label for="name" class="col-md-2 control-label">Nom:</label>
+                                <label for="name" class="col-md-2 control-label">Nom : </label>
                                 <div class="col-md-2">
                                     {{$user->nom}}
                                 </div>
@@ -19,7 +19,7 @@
                         </div>
                         <div class="row">
                             <div class="form-group">
-                                <label for="name" class="col-md-2 control-label">Prénom</label>
+                                <label for="name" class="col-md-2 control-label">Prénom : </label>
                                 <div class="col-md-2">
                                     {{$user->prenom}}
                                 </div>
@@ -27,7 +27,7 @@
                         </div>
                         <div class="row">
                             <div class="form-group">
-                                <label for="name" class="col-md-2 control-label">Email</label>
+                                <label for="name" class="col-md-2 control-label">Email : </label>
                                 <div class="col-md-2">
                                     {{$user->email}}
                                 </div>
@@ -38,16 +38,16 @@
                 </div>
             </div>
 
-            <div class="col-md-7">
+            <div class="col-offset-md-2 col-md-10 right">
                 <div class="panel panel-default">
                 @foreach ($houses as $house)
-                    <div class="panel-heading">Hébergement de {{$house->user->nom}} {{$house->user->prenom}}</div>
                     <div class="panel-body">
                     
                         <div class="row">
                             <div class="col-lg-4 col-md-4 mb-4 thumbnail">
                                 <div class="card h-100">
                                     <img class="img-responsive img_house" src="{{ asset('img/houses/'.$house->photo) }}">
+                                    <p class="panel-heading">Hébergement de {{$house->user->nom}} {{$house->user->prenom}}</p>
                                     <div>
                                         <<h4 class="title card-title">
                                             <a href="{{route('user.showHouse', $house['id']) }}">{{$house->title}}</a>
