9e9050f576633e0826fda5588c1b1373512291bb
Guillaume Montagné <g.montagne777@gmail.com>
2019-02-12 16:04:45 +0100

    Début SEO

--- database/seeds/HousesTableSeeder.php
+++ database/seeds/HousesTableSeeder.php
@@ -13,34 +13,34 @@ class HousesTableSeeder extends Seeder
     {
         $houses = [
             [
-                'title' => 'Cabane',
+                'title' => 'Cabane dans les arbres',
                 'user_id' => 1,
                 'category_id' => 1,
-                'ville' => 'PARIS',
+                'ville' => 'Besançon',
                 'adresse' => '31 rue des peupliers',
-                'description' => 'Super cabane',
+                'description' => 'Cette cabane dans les arbres vous offrira un lieu atypique et chaleureux pour vos vacances en plein air. Ce lieu idylique vous offrira confort et sérénité durant votre voyage.',
                 'price' => 25.00,
                 'photo' => '1523888761.png',
                 'statut' => 'Validé'
             ],
             [
-                'title' => 'Igloo',
+                'title' => 'Igglo ',
                 'user_id' => 2,
                 'category_id' => 2,
                 'ville' => 'LYON',
                 'adresse' => '32 rue mozart',
-                'description' => 'Super igloo',
+                'description' => 'Cette Igglo proche de Lyon, vous offrira le confort et la sécurtié pour votre séjour atypique, seul ou en amoureux. Son toit est ouvert ce qui vous permet de pouvoir dormir le soir à la belle étoile.',
                 'price' => 10.00,
                 'photo' => '1522347485.jpg',
                 'statut' => 'Validé'
             ],
             [
-                'title' => 'Yourte Pays de la Loire - Grande Yourte Mongole',
+                'title' => 'Grande Yourte Mongole',
                 'user_id' => 2,
                 'category_id' => 3,
                 'ville' => 'LOIRE',
                 'adresse' => '12 rue tolbiac',
-                'description' => 'Super yourte',
+                'description' => 'Cette yourte style mongole vous fera voyager dans le pays de la mongolie tout en restant en France, Venez vous évader dans cette nouvelle yourte ou chaleur et confort vienne ce conjuguer à la perfection',
                 'price' => 30.00,
                 'photo' => '1543837576.jpg',
                 'statut' => 'Validé'
